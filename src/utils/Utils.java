e3b4813a461332a915209a0c604d5b23454aabfd
Fernando <fernando.vgarcia@hotmail.com>
2021-06-13 18:49:12 -0300

    Ajustes nos tipos.

--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -1,6 +1,7 @@
 package utils;
 
 public class Utils {
+
     public String firstLetterUppercase(String str) {
         String output = str.substring(0, 1).toUpperCase() + str.substring(1);
 
@@ -9,20 +10,28 @@ public class Utils {
 
     public String tipoAtributo(String tipo) {
         tipo = tipo.toLowerCase();
-        if (tipo.equals("serial") || tipo.equals("integer") || tipo.equals("numeric") || tipo.equals("double precision") || tipo.equals("real") || tipo.equals("int") || tipo.equals("bigint") || tipo.equals("bigserial")) {
+        if (tipo.equals("serial") || tipo.equals("integer") || tipo.equals("numeric") || tipo.equals("int") || tipo.equals("mediumint") || tipo.equals("bigserial")) {
             tipo = "Integer";
+        } else if (tipo.equals("bigint")) {
+            tipo = "Long";
         } else if (tipo.equals("varchar") || tipo.equals("nchar") || tipo.equals("char") || tipo.equals("character")) {
             tipo = "String";
         } else if (tipo.equals("bit") || tipo.equals("boolean")) {
             tipo = "Boolean";
+        } else if (tipo.equals("decimal") || tipo.equals("real") || tipo.equals("double")) {
+            tipo = "Float";
+        } else if (tipo.equals("datetime")) {
+            tipo = "Timestamp";
         }
         return firstLetterUppercase(tipo);
     }
 
     public String tipoAtributoModel(String tipo) {
         tipo = tipo.toLowerCase();
-        if (tipo.equals("serial") || tipo.equals("integer") || tipo.equals("numeric") || tipo.equals("double precision") || tipo.equals("real") || tipo.equals("int") || tipo.equals("bigint") || tipo.equals("bigserial")) {
+        if (tipo.equals("serial") || tipo.equals("integer") || tipo.equals("numeric") || tipo.equals("int") || tipo.equals("mediumint") || tipo.equals("bigserial")) {
             tipo = "Integer";
+        } else if (tipo.equals("bigint")) {
+            tipo = "Long";
         } else if (tipo.equals("varchar") || tipo.equals("nchar") || tipo.equals("char") || tipo.equals("character")) {
             tipo = "String";
         } else if (tipo.equals("bit") || tipo.equals("boolean")) {
@@ -31,9 +40,9 @@ public class Utils {
             tipo = "java.sql.Date";
         } else if (tipo.equals("time")) {
             tipo = "java.sql.Time";
-        } else if (tipo.equals("timestamp")) {
+        } else if (tipo.equals("timestamp") || tipo.equals("datetime")) {
             tipo = "java.sql.Timestamp";
-        } else if (tipo.equals("float")) {
+        } else if (tipo.equals("float") || tipo.equals("decimal") || tipo.equals("real") || tipo.equals("double")) {
             tipo = "Float";
         } else if (tipo.equals("array")) {
             tipo = "ArrayList<E>";
